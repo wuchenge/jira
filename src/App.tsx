fe3998c6f22ecc78e088974d9e67df961fd67b31
无尘阁 <767339576@qq.com>
2021-10-13 16:37:00 +0800

    feat: 添加登录注册

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import { AuthenticatedApp } from "authenticated-app";
+import { useAuth } from "context/auth-context";
 import React from "react";
+import { UnauthenticatedApp } from "unauthenticated-app";
 import "./App.css";
-// import { ProjectListScreen } from "screens/project-list";
-// import { TsReactTest } from "screens/try-use-array";
-import { LoginScreen } from "screens/login";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      {/* <TsReactTest /> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
