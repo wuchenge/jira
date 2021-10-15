2a35bde9972d13e3e2967446088e8736cad1abc5
无尘阁 <767339576@qq.com>
2021-10-15 16:24:34 +0800

    style: grid 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,24 +2,74 @@ import styled from "@emotion/styled";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
+/**
+ * grid 和 flex 各自的应用场景
+ * 1 要考虑 是一维布局 还是 二维布局
+ * 一般来说 一维布局用flex, 二维布局用grid
+ * 2 从内容出发flex 从布局出发 grid
+ * 从内容出发： 你先有一组内容（数量一般不固定），然后希望他们均匀的分布在容器中， 由内容自己的大小决定占据的空间
+ * 从布局出发： 先规划网格（数量一般比较固定），然后再把元素往里填充
+ *
+ */
+
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <PageHeader>
-        <button onClick={logout}>登出</button>
-      </PageHeader>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-    </div>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
 
-const PageHeader = styled.header`
-  height: 6rem;
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
 `;
 
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
 const Main = styled.main`
-  height: calc(100vh - 6rem);
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
 `;
